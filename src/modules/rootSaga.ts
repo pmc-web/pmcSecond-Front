7d8ba96949d981402e93d57c7be0f4dc8390b817
lifeisegg123 <leejj2002@naver.com>
2021-04-11 18:00:58 +0900

    리덕스 제거 & 리액트 쿼리 적용

--- src/modules/rootSaga.ts
+++
@@ -1,7 +0,0 @@
-import { all, fork } from 'redux-saga/effects';
-import sampleSaga from 'src/modules/sample/saga';
-
-function* rootSaga() {
-  yield all([fork(sampleSaga)]);
-}
-export default rootSaga;
