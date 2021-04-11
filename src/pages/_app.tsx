3aea2a712e93113e5e2f6a7bd3ea131dc3cb3f76
lifeisegg123 <leejj2002@naver.com>
2021-04-11 14:49:21 +0900

    글로벌 리셋 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from '@emotion/react';
 import { AppProps } from 'next/app';
 
+import GlobalStyle from 'src/assets/global/style';
 import theme from 'src/assets/theme';
 import wrapper from 'src/modules/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
   );
