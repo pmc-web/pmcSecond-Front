3ca2afc22be6aee60956f52a1fc2bb64826d466c
LEE.SANG.MIN <59109788+SangMinL96@users.noreply.github.com>
2021-04-19 22:15:27 +0900

    validate 아이콘 적용

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/react';
 import { useRef, useState } from 'react';
 import { FieldValues, UseFormRegister, ValidationRule } from 'react-hook-form';
 import theme from 'src/assets/theme';
+import Icon from './Icon';
 import Text from './Text';
 // doc사이트 https://react-hook-form.com/get-started
 type InputProps = {
@@ -12,9 +13,10 @@ type InputProps = {
   pattern?: ValidationRule<RegExp>;
   css?: ReturnType<typeof css>;
   label?: string;
+  icon?: 'success' | 'error';
 };
 
-const Input = ({ name, label, css: inputCss, defaultValue, register, required, pattern }: InputProps) => {
+const Input = ({ icon, name, label, css: inputCss, defaultValue, register, required, pattern }: InputProps) => {
   const focus = useRef<HTMLLabelElement>(null);
   const onFocus = () => {
     if (focus.current) {
@@ -40,26 +42,44 @@ const Input = ({ name, label, css: inputCss, defaultValue, register, required, p
       >
         {label}
       </label>
-      <input
-        id={label}
-        css={(themes) => [
-          inputCss,
-          css`
-            margin-top: 0.5em;
-            border: 1px solid ${themes.color.grey030};
-            padding: 1.05em;
-            border-radius: 6px;
-            color: ${themes.color.black};
-            ${themes.fontSize.subtitle3};
-            &:focus {
-              outline: 0;
-              border: 1px solid ${themes.color.purple050};
-            }
-          `,
-        ]}
-        {...register(name, { required, pattern })}
-        defaultValue={defaultValue}
-      />
+      <div
+        css={css`
+          display: inline;
+          position: relative;
+        `}
+      >
+        <input
+          id={label}
+          css={(themes) => [
+            inputCss,
+            css`
+              margin-top: 0.5em;
+              border: 1px solid ${themes.color.grey030};
+              padding: 1.05em;
+              border-radius: 6px;
+              color: ${themes.color.black};
+              ${themes.fontSize.subtitle3};
+              &:focus {
+                outline: 0;
+                border: 1px solid ${themes.color.purple050};
+              }
+            `,
+          ]}
+          {...register(name, { required, pattern })}
+          defaultValue={defaultValue}
+        />
+        <span
+          css={css`
+            position: absolute;
+            right: 1.5em;
+            top: 50%;
+            transform: translate(50%, -50%);
+          `}
+        >
+          {icon === 'success' ? <Icon name="check" size="18" color="green020" /> : null}
+          {icon === 'error' ? <Icon name="error" size="18" color="red040" /> : null}
+        </span>
+      </div>
     </div>
   );
 };
