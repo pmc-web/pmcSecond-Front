6d6d4d6b1944ce5f2168cbaf6e1ed00d7dd07a3d
lifeisegg <58962402+lifeisegg123@users.noreply.github.com>
2021-04-23 10:12:28 +0900

    버튼 개발 마무리 (#25)
    * 버튼 개발 마무리
    
     - 버튼 개발 마무리
     - 스피너 개발 (스피너 아이콘 교체 필요)
     - 탭 커서 pointer로 변경
     - 현재 버그로 github 이슈 오픈상태인 aslant 옵션 삭제

--- src/assets/theme/index.ts
+++ src/assets/theme/index.ts
@@ -5,9 +5,33 @@ import fontSize from './fontSize';
 const button = {
   primary: {
     'background-color': color.purple050,
-    color: 'white',
+    color: color.white,
     border: 'none',
-    cursor: 'pointer',
+  },
+  default: {
+    'background-color': color.white,
+    color: color.black,
+    border: 'none',
+  },
+  disabled: {
+    'background-color': color.grey030,
+    color: color.purple010,
+    border: 'none',
+  },
+};
+const buttonSize = {
+  large: {
+    'min-height': '3.5rem',
+    'min-width': '23.5rem',
+  },
+  normal: {
+    'min-height': '2.5rem',
+    'min-width': '21rem',
+    'border-radius': '0.5rem',
+  },
+  small: {
+    'min-height': '1.5rem',
+    'min-width': '18.5rem',
   },
 };
 
@@ -23,6 +47,6 @@ const commonStyle = {
   },
 };
 
-const theme: Theme = { color, button, fontSize, commonStyle };
+const theme: Theme = { color, button, buttonSize, fontSize, commonStyle };
 
 export default theme;
---
+++ src/components/common/Spinner.tsx
@@ -0,0 +1,20 @@
+import { css } from '@emotion/react';
+import { rotateClockwise } from 'src/assets/animations/rotate';
+import Icon from './Icon';
+
+type SpinnerProps = {
+  size: string;
+};
+
+const Spinner = ({ size = '20' }: SpinnerProps) => (
+  <Icon
+    css={css`
+      animation: ${rotateClockwise} 0.7s infinite;
+    `}
+    name="alarm" // TODO: 스피너 아이콘 받아서 교체
+    size={size}
+    className="spinner"
+  />
+);
+
+export default Spinner;
