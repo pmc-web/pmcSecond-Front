6b99654501ad279aaecea5590cfdedfd3b296114
LEE.SANG.MIN <59109788+SangMinL96@users.noreply.github.com>
2021-05-19 13:06:47 +0900

    로그인 페이지 (#30)
    * 로그인 페이지 구현
    * 인풋 css -> style & 쿼리 키 관리

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,6 +1,6 @@
 import { css } from '@emotion/react';
-import { useRef } from 'react';
-import { FieldValues, UseFormRegister, ValidationRule } from 'react-hook-form';
+import { useEffect, useRef, useState } from 'react';
+import { FieldValues, UseFormRegister, UseFormSetValue, ValidationRule } from 'react-hook-form';
 import theme from 'src/assets/theme';
 import Icon from './Icon';
 
@@ -8,30 +8,37 @@ import Icon from './Icon';
 type InputProps = {
   name: string;
   register: UseFormRegister<FieldValues>;
+  setValue: UseFormSetValue<FieldValues>;
   defaultValue?: string;
   disabled?: boolean;
   required?: boolean;
   pattern?: ValidationRule<RegExp>;
   style?: ReturnType<typeof css>;
   label?: string;
-  valid?: 'success' | 'error';
+  type?: 'text' | 'password';
+  valid?: 'success' | 'error' | 'close' | 'null';
 };
 
 const Input = ({
   disabled = false,
   valid,
   name,
+  type,
   label,
   style: inputCss,
   defaultValue,
   register,
   required,
   pattern,
+  setValue,
 }: InputProps) => {
+  const [validate, setValiDate] = useState(valid);
+
+  const [close, setClose] = useState(false);
   const focus = useRef<HTMLLabelElement>(null);
   const onChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
     if (ev.target.value !== '') {
-      if (valid !== 'error') {
+      if (validate !== 'error') {
         if (focus.current) {
           focus.current.style.color = theme.color.purple050;
         }
@@ -42,11 +49,12 @@ const Input = ({
         focus.current.style.color = theme.color.grey030;
       }
     }
+    return setClose(true);
   };
 
   const onFocus = (ev: React.FocusEvent<HTMLInputElement>) => {
     if (ev.target.value !== '') {
-      if (valid !== 'error') {
+      if (validate !== 'error') {
         if (focus.current) {
           focus.current.style.color = theme.color.purple050;
         }
@@ -58,7 +66,22 @@ const Input = ({
     if (focus.current) {
       focus.current.style.color = theme.color.grey030;
     }
+    setTimeout(() => setClose(false), 100);
   };
+
+  const onVaildate = () => {
+    if (validate === 'success') return <Icon name="check" size="18" color="green020" />;
+    if (validate === 'error') return <Icon name="error" size="18" color="red040" />;
+    return null;
+  };
+
+  const onClose = () => {
+    setValiDate('null');
+    setValue(name, '');
+  };
+  useEffect(() => {
+    setValiDate(valid);
+  }, [valid]);
   return (
     <div onChange={onChange} onFocus={onFocus} onBlur={onBlur}>
       <label
@@ -79,17 +102,19 @@ const Input = ({
       >
         <input
           id={label}
+          type={type}
           css={(themes) => [
             css`
+              width: 100%;
               margin-top: 0.5em;
-              border: 2px solid ${valid !== 'error' ? themes.color.grey030 : themes.color.red040};
+              border: 2px solid ${validate !== 'error' ? themes.color.grey030 : themes.color.red040};
               padding: 1.05em;
               border-radius: 6px;
               color: ${themes.color.black};
               ${themes.fontSize.subtitle3};
               &:focus {
                 outline: 0;
-                border: 2px solid ${valid !== 'error' ? themes.color.purple050 : themes.color.red040};
+                border: 2px solid ${validate !== 'error' ? themes.color.purple050 : themes.color.red040};
               }
             `,
             inputCss,
@@ -106,8 +131,20 @@ const Input = ({
             transform: translate(50%, -50%);
           `}
         >
-          {valid === 'success' ? <Icon name="check" size="18" color="green020" /> : null}
-          {valid === 'error' ? <Icon name="error" size="18" color="red040" /> : null}
+          {close ? (
+            <span
+              role="button"
+              onClick={onClose}
+              tabIndex={0}
+              css={css`
+                cursor: pointer;
+              `}
+            >
+              <Icon name="closeBox" size="18" color="grey030" />
+            </span>
+          ) : (
+            onVaildate()
+          )}
         </span>
       </div>
     </div>
