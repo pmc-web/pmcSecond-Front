0a7bb44049536b9f7e90dba1a6e01305565bc2c3
yeongfree1 <yeongfree1@gmail.com>
2021-06-06 20:57:18 +0900

    Merge

---
+++ src/pages/setting/index.tsx
@@ -0,0 +1,76 @@
+import { css } from '@emotion/react';
+import TopBar from 'src/components/common/layout/TopBar';
+import SideBarItem from 'src/components/common/layout/SideBarItem';
+import { useCallback } from 'react';
+
+const SettingIndex = () => {
+  const onAuthInfo = useCallback(() => {
+    console.log('회원정보');
+  }, []);
+  const onAddressAdmin = useCallback(() => {
+    console.log('주소관리');
+  }, []);
+  return (
+    <section>
+      {/* 헤더 */}
+      <TopBar title="개인정보 수정" leftIcon="leftArrow" />
+      {/* 유저 정보 */}
+      <div
+        css={(theme) => css`
+          display: flex;
+          align-items: center;
+          background-color: ${theme.color.white};
+          padding: 20px;
+        `}
+      >
+        <div
+          css={(theme) =>
+            css`
+              width: 48px;
+              height: 48px;
+              margin-right: 20px;
+              background-color: ${theme.color.grey020};
+              border-radius: 100%;
+            `
+          }
+        />
+        <div>
+          <p
+            css={(theme) =>
+              css`
+                ${theme.fontSize.body1}
+                margin-bottom: 3px;
+                color: ${theme.color.grey070};
+              `
+            }
+          >
+            부여번호
+          </p>
+          <p
+            css={(theme) =>
+              css`
+                ${theme.fontSize.body1}
+              `
+            }
+          >
+            info@marketuniverse.co.kr
+          </p>
+        </div>
+      </div>
+      {/* 라인 */}
+      <div
+        css={(theme) => css`
+          height: 10px;
+          background-color: ${theme.color.grey020};
+          border-top: 1px solid ${theme.color.grey030};
+          border-bottom: 1px solid ${theme.color.grey030};
+        `}
+      />
+      {/* 회원정보 수정 & 주소관리 */}
+      <SideBarItem label="회원정보" onClick={onAuthInfo} rightIcon="rightArrow" />
+      <SideBarItem label="주소관리" onClick={onAddressAdmin} rightIcon="rightArrow" />
+    </section>
+  );
+};
+
+export default SettingIndex;
