7ea7c5891f5e66327819501668f6e58aa6de57df
yeongfree1 <yeongfree1@gmail.com>
2021-06-12 17:17:09 +0900

    회원정보 수정 및 비밀번호변경 UI 생성

--- src/pages/setting/changeInfo.tsx
+++ src/pages/setting/changeInfo.tsx
@@ -1,13 +1,15 @@
 import { css } from '@emotion/react';
-import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/router';
+import { useForm, DeepMap, FieldError } from 'react-hook-form';
 import Button from 'src/components/common/Button';
 import Input from 'src/components/common/Input';
 import TopBar from 'src/components/common/layout/TopBar';
 
 type FormData = {
   email?: string;
+  nickname?: string;
+  phone?: string;
   pw?: string;
-  pwCheck?: string;
 };
 
 const ChangeInfo = () => {
@@ -18,13 +20,25 @@ const ChangeInfo = () => {
     setValue,
     formState: { errors },
   } = useForm<FormData>({ mode: 'onChange' });
+  const router = useRouter();
+
+  const onValid = (name: keyof DeepMap<FormData, FieldError>) => {
+    if (errors[name]) {
+      return 'error';
+    }
+    return 'success';
+  };
+  // 비밀번호 수정 페이지 이동
+  const onMoveToChangePw = () => {
+    router.push('/setting/changePw');
+  };
 
   return (
     <>
       <TopBar title="회원정보 수정" leftIcon="leftArrow" />
       <section
         css={(themes) => css`
-          padding: 0 1.2em;
+          padding: 30px 1.2em 0 1.2em;
           height: 100%;
           ${themes.commonStyle.flexColomn};
           justify-content: space-between;
@@ -40,17 +54,45 @@ const ChangeInfo = () => {
             margin-bottom: 1.875em;
           `}
         />
-        <Input
-          required
-          label="닉네임"
-          name="nickname"
-          setValue={setValue}
-          register={register}
-          placeholder="부여번호"
-          style={css`
+        {/* 닉네임 */}
+        <div
+          css={css`
+            display: flex;
+            /* justify-content: space-between; */
+            align-items: flex-end;
             margin-bottom: 1.875em;
+            width: 100%;
           `}
-        />
+        >
+          <div
+            css={css`
+              flex: 1;
+            `}
+          >
+            <Input
+              required
+              label="닉네임"
+              name="nickname"
+              setValue={setValue}
+              register={register}
+              placeholder="부여번호"
+            />
+          </div>
+          <Button
+            htmlType="button"
+            type="primary"
+            style={css`
+              border-radius: 5px;
+              min-height: 3.3rem;
+              margin-left: 1rem;
+              width: 97px;
+            `}
+            // disabled={(!checked.Term1 || !checked.Term2) && true}
+            // onClick={onRouter}
+          >
+            중복학인
+          </Button>
+        </div>
         <Input
           required
           label="연락처"
@@ -62,15 +104,53 @@ const ChangeInfo = () => {
             margin-bottom: 1.875em;
           `}
         />
-        <Input
-          required
-          label="비밀번호"
-          name="pw"
-          setValue={setValue}
-          register={register}
-          placeholder="기존 비밀번호를 입력해주세요."
-        />
+
+        {/* 닉네임 */}
+        <div
+          css={css`
+            display: flex;
+            /* justify-content: space-between; */
+            align-items: flex-end;
+            margin-bottom: 1.875em;
+            width: 100%;
+          `}
+        >
+          <div
+            css={css`
+              flex: 3;
+            `}
+          >
+            <Input
+              required
+              label="비밀번호"
+              name="pw"
+              type="password"
+              setValue={setValue}
+              register={register}
+              placeholder="기존 비밀번호를 입력해주세요."
+              valid={!getValues('pw') ? 'null' : onValid('pw')}
+              pattern={/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/}
+            />
+          </div>
+          <Button
+            htmlType="button"
+            type="primary"
+            onClick={onMoveToChangePw}
+            style={css`
+              border-radius: 5px;
+              min-height: 3.3rem;
+              margin-left: 1rem;
+              flex: 1;
+            `}
+            disabled={!getValues('pw')}
+            // onClick={onRouter}
+          >
+            수정
+          </Button>
+        </div>
       </section>
+
+      {/* 완료 버튼 */}
       <Button
         htmlType="button"
         size="large"
---
+++ src/pages/setting/changePw.tsx
@@ -0,0 +1,85 @@
+import { css } from '@emotion/react';
+import { DeepMap, FieldError, useForm } from 'react-hook-form';
+import TopBar from 'src/components/common/layout/TopBar';
+import Input from 'src/components/common/Input';
+import Button from 'src/components/common/Button';
+
+type FormData = {
+  pw?: string;
+  pwCheck?: string;
+};
+
+const ChangePw = () => {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>({ mode: 'onChange' });
+
+  const onValid = (name: keyof DeepMap<FormData, FieldError>) => {
+    if (errors[name]) {
+      return 'error';
+    }
+    if (name === 'pwCheck') {
+      if (getValues('pw') === getValues('pwCheck')) {
+        return 'success';
+      }
+      return 'error';
+    }
+    return 'success';
+  };
+
+  return (
+    <>
+      <TopBar title="비밀번호 수정" leftIcon="leftArrow" />
+      <section
+        css={(themes) => css`
+          ${themes.commonStyle.flexColomn};
+          padding: 30px 1.2em 0 1.2em;
+          height: 100%;
+          justify-content: space-between;
+        `}
+      >
+        <Input
+          required
+          label="새로운 비밀번호"
+          name="pw"
+          type="password"
+          register={register}
+          setValue={setValue}
+          valid={!getValues('pw') ? 'null' : onValid('pw')}
+          pattern={/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/}
+          style={css`
+            margin-bottom: 1.875em;
+          `}
+        />
+        <Input
+          required
+          type="password"
+          label="새로운 비밀번호 확인"
+          name="pwCheck"
+          register={register}
+          setValue={setValue}
+          valid={!getValues('pwCheck') ? 'null' : onValid('pwCheck')}
+          pattern={new RegExp(String(getValues('pw')))}
+        />
+      </section>
+      <Button
+        style={css`
+          position: fixed;
+          bottom: 0;
+        `}
+        htmlType="submit"
+        size="large"
+        type="primary"
+        disabled={onValid('pw') !== 'success' || onValid('pwCheck') !== 'success'}
+      >
+        완료
+      </Button>
+    </>
+  );
+};
+
+export default ChangePw;
