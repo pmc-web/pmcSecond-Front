137e2aa93ebefe41abbedbd8fdf608ea099a88b7
sangmin.lee <hwon3794@gmail.com>
2021-07-06 23:20:31 +0900

    로그인 | 회원가입 API 및 axios 전역설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import { useRef } from 'react';
 import { AppProps } from 'next/app';
+import axios from 'axios';
 import { ThemeProvider } from '@emotion/react';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query/hydration';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
 import GlobalStyle from 'src/assets/global/style';
 import theme from 'src/assets/theme';
 
+axios.defaults.baseURL = process.env.API_URL;
+axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers['Access-Control-Allow-Credentials'] = true;
 function App({ Component, pageProps }: AppProps) {
   const queryClientRef = useRef<null | QueryClient>(null);
   if (!queryClientRef.current) {
