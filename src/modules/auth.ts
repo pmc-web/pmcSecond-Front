cd480465e4ef14896fda4439e59fed63c139a3ff
LEE.SANG.MIN <59109788+SangMinL96@users.noreply.github.com>
2021-07-11 15:39:59 +0900

    로그인 | 회원가입 API 및 axios 전역설정 (#44)

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 import { useMutation, useQuery } from 'react-query';
 import { loginTypes } from 'src/interfaces/auth';
 
@@ -7,14 +8,15 @@ const loginKey = () => ['auth', 'login'];
 const userKey = () => ['auth', 'user'];
 
 export const getSignUpApi = async (data: loginTypes) => {
-  const res = await axios.post(`${process.env.API_URL}/users/sign-up`, data);
+  const res = await axios.post(`/users/sign-up`, data);
   return res.data;
 };
 
 export const useSignUpMutation = () => useMutation(signUpKey(), getSignUpApi);
 
 const getLoginApi = async (data: loginTypes) => {
-  const res = await axios.post(`${process.env.API_URL}/user/users/login`, data);
+  const res = await axios.post(`/users/login`, data);
+  console.log(res);
   return res.data.data;
 };
 
