a8014ec6adc69676fe655e29b9cc6d420156d438
lifeisegg123 <leejj2002@naver.com>
2021-07-29 23:14:11 +0900

    회원가입 & 이메일 로그인 라우팅 처리, 에러 처리

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -5,6 +5,7 @@ import Input from 'src/components/common/Input';
 import TopBar from 'src/components/common/layout/TopBar';
 import { useRouter } from 'next/router';
 import { useSignUpMutation } from 'src/modules/auth';
+import Layout from 'src/components/common/layout/Layout';
 
 type FormData = {
   email?: string;
@@ -22,11 +23,17 @@ const SignUp = () => {
   } = useForm<FormData>({ mode: 'onChange' });
   const mutation = useSignUpMutation();
 
-  const onSubmit = async (data: FormData) => {
+  const onSubmit = async (data: FormData): Promise<void> => {
     if (data.email && data.pw) {
       const postData = { email: data.email, password: data.pw };
-      const result = mutation.mutate(postData);
-      return result;
+      try {
+        await mutation.mutateAsync(postData);
+        window.alert('이메일 인증이 필요합니다. 이메일을 확인해 주세요.');
+        setTimeout(() => router.push('/auth/emailLogin'), 0);
+      } catch (error) {
+        console.error(error);
+        window.alert(error.response?.data.message);
+      }
     } else {
       window.alert('빈칸이 존재합니다.');
     }
@@ -44,95 +51,91 @@ const SignUp = () => {
     }
     return 'success';
   };
-  const onClick = (name: string) => () => {
-    if (name === 'leftArrow') {
-      router.push('/auth/terms');
-    }
-  };
 
   return (
-    <form
-      css={css`
-        width: 100%;
-      `}
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <TopBar onClick={onClick} leftIcon="leftArrow" title="회원가입" />
-      <section
-        css={(themes) => css`
-          height: 100%;
-          ${themes.commonStyle.flexColomn};
-          justify-content: space-between;
+    <Layout leftIcon="leftArrow" title="회원가입">
+      <form
+        css={css`
+          width: 100%;
         `}
+        onSubmit={handleSubmit(onSubmit)}
       >
-        <h1
-          css={(themes) =>
-            css`
-              padding: 0 1em;
-              margin-top: 2em;
-              margin-bottom: -5em;
-              ${themes.fontSize.h1};
-            `
-          }
-        >
-          회원가입에 필요한
-          <br />
-          정보를 입력해주세요.
-        </h1>
-
-        <div
+        <section
           css={(themes) => css`
+            height: 100%;
             ${themes.commonStyle.flexColomn};
             justify-content: space-between;
-            margin-top: 15em;
-            height: 20em;
-            padding: 0 1.2em;
           `}
         >
-          <Input
-            required
-            label="이메일"
-            name="email"
-            setValue={setValue}
-            valid={!getValues('email') ? 'null' : onValid('email')}
-            pattern={/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i}
-            register={register}
-          />
-          <Input
-            required
-            label="비밀번호"
-            name="pw"
-            type="password"
-            valid={!getValues('pw') ? 'null' : onValid('pw')}
-            pattern={/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/}
-            register={register}
-            setValue={setValue}
-          />
-          <Input
-            valid={!getValues('pwCheck') ? 'null' : onValid('pwCheck')}
-            pattern={new RegExp(String(getValues('pw')))}
-            required
-            type="password"
-            label="비밀번호 확인"
-            name="pwCheck"
-            register={register}
-            setValue={setValue}
-          />
-        </div>
+          <h1
+            css={(themes) =>
+              css`
+                padding: 0 1em;
+                margin-top: 2em;
+                margin-bottom: -5em;
+                ${themes.fontSize.h1};
+              `
+            }
+          >
+            회원가입에 필요한
+            <br />
+            정보를 입력해주세요.
+          </h1>
 
-        <Button
-          style={css`
-            position: fixed;
-            bottom: 0;
-          `}
-          htmlType="submit"
-          size="large"
-          type="primary"
-        >
-          가입하기
-        </Button>
-      </section>
-    </form>
+          <div
+            css={(themes) => css`
+              ${themes.commonStyle.flexColomn};
+              justify-content: space-between;
+              margin-top: 15em;
+              height: 20em;
+              padding: 0 1.2em;
+            `}
+          >
+            <Input
+              required
+              label="이메일"
+              name="email"
+              setValue={setValue}
+              valid={!getValues('email') ? 'null' : onValid('email')}
+              pattern={/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i}
+              register={register}
+            />
+            <Input
+              required
+              label="비밀번호"
+              name="pw"
+              type="password"
+              valid={!getValues('pw') ? 'null' : onValid('pw')}
+              pattern={/^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,16}$/}
+              register={register}
+              setValue={setValue}
+            />
+            <Input
+              valid={!getValues('pwCheck') ? 'null' : onValid('pwCheck')}
+              pattern={new RegExp(String(getValues('pw')))}
+              required
+              type="password"
+              label="비밀번호 확인"
+              name="pwCheck"
+              register={register}
+              setValue={setValue}
+            />
+          </div>
+
+          <Button
+            style={css`
+              position: fixed;
+              bottom: 0;
+            `}
+            htmlType="submit"
+            size="large"
+            type="primary"
+          >
+            가입하기
+          </Button>
+        </section>
+      </form>
+    </Layout>
   );
 };
 
